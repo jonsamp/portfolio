ac7773bb4a9368066f4e9dd286074cdbf001babd
Jon Samp <sampjon@gmail.com>
2017-05-07 16:29:13 -0400

    Embed entire.life with better height

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 var userFeed = new Instafeed({
-    get: 'user',
-    userId: '17942230',
-    accessToken: '17942230.e281b92.8252eb3acca645b5a37b86388ee59137',
-    limit: 1,
-    resolution: 'standard_resolution',
-    template: '<img src="{{image}}" class="photo"/><div class="caption"><p>@{{location}}: {{caption}}</p></div>'
+  get: 'user',
+  userId: '17942230',
+  accessToken: '17942230.e281b92.8252eb3acca645b5a37b86388ee59137',
+  limit: 1,
+  resolution: 'standard_resolution',
+  template: '<img src="{{image}}" class="photo"/><div class="caption"><p>@{{location}}: {{caption}}</p></div>'
 });
+
+
+function adjustHeight(message) {
+  var origin = message.origin;
+  var event = message.data.event;
+  var height = message.data.height;
+  if (origin === 'https://entire.life' && event === 'ENTIRELIFE_ROUTE_CHANGE') {
+    var iframe = document.querySelector('#entire-life-calendar');
+    iframe.height = '' + (height + 5) + 'px';
+  }
+}
+
 userFeed.run();
+window.addEventListener('message', adjustHeight, false);
